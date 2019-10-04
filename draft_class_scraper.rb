4d56b54701efa4ede8b1b9ddfd30671b7444a429
Oracle Public Cloud User <opc@sportsdata-dev.subnet1.sportsdata.oraclevcn.com>
2019-10-04 17:04:27 +0000

    testing with csvs

--- draft_class_scraper.rb
+++ draft_class_scraper.rb
@@ -1,17 +1,18 @@
 require 'open-uri'
 require 'nokogiri'
 require 'csv'
+require 'pry'
 
 def draft_class_scraper(start_year)
 
-	year = start_year
+	@year = start_year
 
 	while true
-		url = "https://www.pro-football-reference.com/years/#{year}/draft.htm"
+		url = "https://www.pro-football-reference.com/years/#{@year}/draft.htm"
 
-		draft_class_parser(url)
-		puts "#{year} complete"
-		year += 1
+		draft_class_parser(open(url))
+		puts "#{@year} complete"
+		@year += 1
 	end
 
 end		
@@ -34,7 +35,8 @@ def draft_class_parser(html)
 		if tr.children[0].text == "\n         "  && drafted_players.count == 0
 
                 	drafted_players.push(
-                	round: tr.children[1].text,
+                	year: @year,
+			round: tr.children[1].text,
                 	pick: tr.children[3].text,
                		team: tr.children[5].text,
                 	player_name: tr.children[7].text,
@@ -48,7 +50,8 @@ def draft_class_parser(html)
 		elsif tr.children[0].text != "\n         "
 
 	                drafted_players.push(
-	                round: tr.children[0].text,
+	                year: @year,
+			round: tr.children[0].text,
 	                pick:  tr.children[1].text,
 	                team: tr.children[2].text,
 	                player_name: tr.children[3].text,
@@ -63,7 +66,7 @@ def draft_class_parser(html)
 
 	end
 
-	CSV.open("#{year}_drafted_players.csv", 'wb') do |csv|
+	CSV.open("#{@year}_drafted_players.csv", 'wb') do |csv|
 		drafted_players.each do |hash|
 			csv << hash.values
 		end
