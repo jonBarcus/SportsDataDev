1e734f437fcdb001ebe79c2a3099cfed9d3a016a
Oracle Public Cloud User <opc@sportsdata-dev.subnet1.sportsdata.oraclevcn.com>
2019-10-09 21:20:21 +0000

    update db_test.rb to be a test template

--- db_test.rb
+++ db_test.rb
@@ -1,7 +1,19 @@
 require 'oci8'
 require 'pry'
 
+# must execute this .rb with "SUDO -E" prepended to use oci8 and environmental variables
 
-dbconnect = OCI8.new($SPORTSDBUSER,$SPORTSDATADEV_PASS,$SPORTSDB_HOSTNAME)
+dbconnect = OCI8.new("#{ENV['SPORTSDBUSER']}/#{ENV['SPORTSDATADEV_PASS']}@//#{ENV['SPORTSDB_HOSTNAME']}:1512/xe")
 
-binding.pry
+dbconnect.exec("CREATE TABLE states (
+		id CHAR(2) PRIMARY KEY,
+		name VARCHAR2(15) NOT NULL,
+		capital VARCHAR2(25) NOT NULL)")
+
+sqlInsert = "INSERT INTO states (id, name, capital) values (:id, :name, :capital)"
+
+dbconnect.exec(sqlInsert, "MA", "Massachusetts", "Boston")
+dbconnect.commit
+
+dbconnect.logoff
+
